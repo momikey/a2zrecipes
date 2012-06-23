3f88f62b7b5ef86ea7e4d10eba894f11b2c34fe7
Michael Potter <mpotter83@gmail.com>
2012-06-23 03:07:58 -0400

    Added license/copyright notices, reformatted XML, added tutorials

--- src/net/potterpcs/recipebook/ImportFileListActivity.java
+++ src/net/potterpcs/recipebook/ImportFileListActivity.java
@@ -1,3 +1,25 @@
+/*
+ *    Copyright 2012 Michael Potter
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ */
+
+/*
+ * ImportFileListActivity.java - Simple file display for app-specific files,
+ * used to work around device file managers that don't send actions properly. 
+ */
+
 package net.potterpcs.recipebook;
 
 import java.io.File;
